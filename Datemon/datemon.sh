4ac3150feea4e0c8b025b0e8846f346908e6e551
TsukiGva2 <tsukigva@gmail.com>
2024-08-30 14:11:45 -0300

    fix: agora o sexo é real

---
+++ Datemon/datemon.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# XXX: USE OF LOCALTIME
+while true; do
+
+	echo "Pinging $READER_IP"
+
+	if ping $READER_IP; then
+		echo "Setting reader clock: $(date +%H:%M:%S)"
+		(sleep 1; echo root; sleep 1; echo impinj; sleep 1; echo "config system time $(date +'%Y.%m.%d-%H:%M:%S')"; sleep 1; echo exit) | telnet $READER_IP
+	fi
+
+	sleep 40 # wow
+done
--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # MonoTempo
+
 Centralized stuff
 
 # Tasks
 
 - [X] Configure network automatically
+
+## Next minor version
+
+### In progress
+
+- [ ] Install script
+
+### Assigned
+
 - [ ] Start container and stuff automatically at boot
 - [ ] Configure impinj reader's time automatically
 - [ ] Configure docker-compose.yml to the correct reader
