bcc9f96a8834e79263dd828c634cc4d07edbbac7
tsukigva <tsukigva@gmail.com>
2024-08-30 17:02:15 -0300

    NAO TEM O AUTOCOMMIT NAO TEM O AUTO COMMMMTTTT

--- Datemon/datemon.sh
+++ Datemon/datemon.sh
@@ -5,7 +5,7 @@ while true; do
 
 	echo "Pinging $READER_IP"
 
-	if ping $READER_IP; then
+	if ping -c 1 $READER_IP; then
 		echo "Setting reader clock: $(date +%H:%M:%S)"
 		(sleep 1; echo root; sleep 1; echo impinj; sleep 1; echo "config system time $(date +'%Y.%m.%d-%H:%M:%S')"; sleep 1; echo exit) | telnet $READER_IP
 	fi
