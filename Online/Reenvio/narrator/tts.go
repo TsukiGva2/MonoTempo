5ed88e712f2a6ac75cec341cb05bb900c1c7cbc0
Rodrigo Monteiro Junior <71048858+TsukiGva2@users.noreply.github.com>
2025-05-07 11:35:10 -0300

    fix: comment out logging statements in Say function

--- Online/Reenvio/narrator/tts.go
+++ Online/Reenvio/narrator/tts.go
@@ -1,7 +1,6 @@
 package narrator
 
 import (
-	"log"
 	"net/http"
 	"net/url"
 )
@@ -13,19 +12,19 @@ func Say(s string) {
 	params.Add("text", s)
 
 	finalURL := baseURL + "?" + params.Encode()
-	log.Println("finalURL:", finalURL)
+	//log.Println("finalURL:", finalURL)
 
 	resp, err := http.Get(finalURL)
 	if err != nil {
-		log.Println("Error:", err)
+		//log.Println("Error:", err)
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		log.Println("HTTP error status:", resp.Status)
+		//log.Println("HTTP error status:", resp.Status)
 		return
 	}
 
-	log.Println("Request successfully sent")
+	//log.Println("Request successfully sent")
 }
